2b02615e031778a07830411b60ce2cfcae05f69b
OzoneBht1 <np03cs4s210036@heraldcollege.edu.np>
2023-02-04 16:04:09 +0545

    React Boilerplate

---
+++ clone/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
